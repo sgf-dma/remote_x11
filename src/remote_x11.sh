4986cebca3344d3f72981f9ac5427e336a74c3f1
sgf <sgf.dma@gmail.com>
2014-08-21 14:22:23 +0400

    remote_x11. Socat version of running X11 clients remotely through ssh (final).
    Working and final version of script for running remote X clients with ssh and
    socat.

--- src/remote_x11.sh
+++ src/remote_x11.sh
@@ -2,37 +2,11 @@
 
 set -euf
 
-### Library
-
-# Retry to run command in background, until it succeedes. If it fails (exits
-# with >0), run step function to change some resource, and retry to run
-# the command. Note, that retry itself runs in the background. To terminate,
-# kill entire process group.
-# 1 - step function.
-# 2.. - cmd to retry (run through eval).
-retry()
-{
-    local step="$1"
-    shift 1
-
-    while [ 0 ]; do
-	eval "$@" &
-	if wait "$!"; then
-	    break
-	else
-	    eval "$step"
-	fi
-    done &
-}
-
-
-### Main
-
 host="$1"
-cmd="$2"
+shift
 
 if [ "x$DISPLAY" = x ]; then
-    echo "Error: DISPLAY variable not set."
+    echo "Error: DISPLAY variable not set." 1>&2
     exit 1
 fi
 
@@ -40,45 +14,54 @@ fi
 ndisp="${DISPLAY#*:}"	# Remove hostname.
 ndisp="${ndisp%.*}"	# Should i remove screen number?
 
-sock="/tmp/.X11-unix/X$ndisp"
+# Find unused local port. This check skips only ports, where someone listens,
+# but if port is in use due to some error (e.g. previous socat instance was
+# terminated abnormally), the check will think the port is free.
+lport=$((6000 + $ndisp + 1))
+while [ 0 ]; do
+    echo "Trying to local port $lport.." 1>&2
+    if socat OPEN:/dev/null TCP:127.0.0.1:$lport 2>/dev/null; then
+	lport=$(($lport + 1))
+    else
+	break
+    fi
+done
+
+sock="/tmp/.X11-unix/X$ndisp"	# Current display's unix domain socket..
+dport=$((6000 + $ndisp))	# .. and TCP port.
 if [ -S "$sock" ]; then
-    echo "Connect to unix domain socket '$sock'."
-    lport=$((6000 + $ndisp + 1))
-    retry 'lport=$(($lport + 1));' \
-	  'echo "Try to listen on port $lport.."
-	   socat TCP-LISTEN:$lport,bind=127.0.0.1 UNIX-CONNECT:"$sock"
-	  '
+    echo "Listen on port $lport and connect to unix domain socket '$sock'." 1>&2
+    socat TCP-LISTEN:$lport,bind=127.0.0.1 UNIX-CONNECT:"$sock" &
+elif socat OPEN:/dev/null TCP:127.0.0.1:$dport 2>/dev/null; then
+    echo "Listen on port $lport and connect to TCP port $dport." 1>&2
+    socat TCP-LISTEN:$lport,bind=127.0.0.1 TCP:127.0.0.1:$dport &
 else
-    # Check, that someone really listens on $port?
-    dport=$((6000 + $ndisp))
-    echo "Connect to TCP port $dport."
-    retry 'lport=$(($lport + 1));' \
-	  'echo "Try to listen on port $lport.."
-	   socat TCP-LISTEN:$lport,bind=127.0.0.1 TCP:127.0.0.1:$dport
-	  '
+    echo "Error: can't connect to X server on display '$ndisp'". 1>&2
+    exit 1
 fi
-retry_pid="$!"
-retry_pgid="$(ps --no-headings -p "$retry_pid" -o pgid \
-		| sed -e's/[[:space:]]//g')"
-trap "/bin/kill -TERM -\"\$retry_pgid\" 2>/dev/null" INT QUIT EXIT 
+socat_pid="$!"
+trap "/bin/kill -HUP \"\$socat_pid\" 2>/dev/null" INT QUIT EXIT 
 
 # Or add to key: command="sockauth=\"$(mktemp --tmpdir sockauth.XXXXXX)\"; export XAUTHORITY=\"$sockauth\"; export DISPLAY=127.0.0.1:9; xauth nmerge -; eval \"$SSH_ORIGINAL_COMMAND\"" 
-# Hardcoded port on remote side.
-
 remoteauth="$(mktemp --tmpdir remoteauth.XXXXXX)"
+# Hardcoded port on remote side.
+rport=6009
+rdisp=9
 xauth -f "$remoteauth" generate :0 . trusted timeout 50
 xauth -f "$remoteauth" nlist \
     | cut -d' ' -f9 \
-    | ssh -R 6009:127.0.0.1:$lport "$host" \
+    | ssh -C -R 6009:127.0.0.1:$lport "$host" \
 	"remoteauth=\"\$(mktemp --tmpdir remoteauth.XXXXXX)\"
 	 export XAUTHORITY=\"\$remoteauth\"
-	 export DISPLAY=127.0.0.1:9
+	 export DISPLAY=127.0.0.1:$rdisp
 	 read d
-	 xauth add ':9' . \"\$d\"
-	 xauth list
-	 echo \$DISPLAY
-	 $cmd
-	 #rm -f \"\$remoteauth\"
+	 xauth add ':$rdisp' . \"\$d\"
+	 $*
+	 rm -f \"\$remoteauth\"
 	 "
+# When ssh runs in the background (with '-f'), command it runs can't read
+# stdin.
+
+rm -f "$remoteauth"
 # socat will be killed by `trap` action,
 
