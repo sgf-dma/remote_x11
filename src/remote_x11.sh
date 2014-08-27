7908ecbe8364f6030a8764d7063223bf4d214b92
sgf <sgf.dma@gmail.com>
2014-08-27 22:53:00 +0400

    remote_x11. Major rewrite. Handle several single connections correctly.
        - Handle several connections by different script instances correctly (use
          different local port and increment remote port correspondingly).
        - Check, that local port is not used, and that X server listens on TCP
          port.
        - Prefer TCP connections to X server over unix domain socket, becuase user
          calling this script will much likely will be allowed in `xhost`, which
          results in no checks on MIT cookie for remote X client at all.

--- src/remote_x11.sh
+++ src/remote_x11.sh
@@ -2,6 +2,9 @@
 
 set -euf
 
+readonly True=1
+readonly False=0
+
 host="$1"
 shift
 
@@ -10,58 +13,113 @@ if [ "x$DISPLAY" = x ]; then
     exit 1
 fi
 
-# Display number.
-ndisp="${DISPLAY#*:}"	# Remove hostname.
-ndisp="${ndisp%.*}"	# Should i remove screen number?
+#readonly use_cookie="$True"  # Use MIT cookie or not.
+use_cookie="$True"
 
-# Find unused local port. This check skips only ports, where someone listens,
-# but if port is in use due to some error (e.g. previous socat instance was
-# terminated abnormally), the check will think the port is free.
-lport=$((6000 + $ndisp + 1))
-while [ 0 ]; do
-    echo "Trying to local port $lport.." 1>&2
-    if socat OPEN:/dev/null TCP:127.0.0.1:$lport 2>/dev/null; then
-	lport=$(($lport + 1))
-    else
+# Display number.
+ndisp="${DISPLAY#*:}"	# Remove hostname..
+ndisp="${ndisp%.*}"	# ..and remove screen number.
+dport=$((6000 + $ndisp))    # Display TCP port.
+sock="/tmp/.X11-unix/X$ndisp"	# X server (local) unix domain socket.
+
+disp_inc=1	# Increment for display number.
+lport=$(($dport + $disp_inc))	# Listening (local) socat port.
+readonly lport_max=6030	    # Maximum socat port number.
+
+socat_pid=''	# background socat process PID.
+
+remoteauth=''	# mit-magic-cookie generated for remote auth.
+
+rdisp=10	# Remote display to use.
+rport=$((6000 + $rdisp))    # Remote port to forward.
+
+trap 'rm -vf "$remoteauth"; /bin/kill "$socat_pid" 2>/dev/null;' \
+     INT QUIT EXIT 
+
+# Find unused local port.
+while [ $lport -lt $lport_max ]; do
+    echo "Trying local port $lport.." 1>&2
+    # It seems, that without '-u' option i can't reuse port immediately. I.e.
+    # unused port, which i'd found here, becomes "already in use", when i'll
+    # try to bind to it in following socat (which redirects ssh connection
+    # forwarding to X11).
+    socat -u TCP-LISTEN:$lport,bind=127.0.0.1 OPEN:/dev/null 2>/dev/null &
+    sleep 1
+    nc -z 127.0.0.1 $lport || true
+    if wait "$!"; then
 	break
     fi
+    disp_inc=$(($disp_inc + 1))
+    lport=$(($dport + $disp_inc))
 done
+if [ $lport -eq $lport_max ]; then
+    echo "Error: Can't find unused local port." 1>&2
+    exit 1
+fi
 
-sock="/tmp/.X11-unix/X$ndisp"	# Current display's unix domain socket..
-dport=$((6000 + $ndisp))	# .. and TCP port.
-if [ -S "$sock" ]; then
-    echo "Listen on port $lport and connect to unix domain socket '$sock'." 1>&2
-    socat TCP-LISTEN:$lport,bind=127.0.0.1 UNIX-CONNECT:"$sock" &
-elif socat OPEN:/dev/null TCP:127.0.0.1:$dport 2>/dev/null; then
+# I prefer connecting to X server through TCP, because if 127.0.0.1 is not
+# allowed in `xhost` (which is usual), remote X client can connect only
+# with correct MIT cookie. Otherwise, if socat connects to unix domain socket and
+# user running socat (and this script) is allowed in `xhost` (which is default
+# for many distributions), remote X client does not need MIT cookie at all and
+# will always pass through, because X server will check socat uid/gid on
+# connection.
+if [ "$use_cookie" = "$True" ] \
+   && socat -u OPEN:/dev/null TCP:127.0.0.1:$dport 2>/dev/null;
+then
     echo "Listen on port $lport and connect to TCP port $dport." 1>&2
     socat TCP-LISTEN:$lport,bind=127.0.0.1 TCP:127.0.0.1:$dport &
+elif [ -S "$sock" ]; then
+    echo "Listen on port $lport and connect to unix domain socket '$sock'." 1>&2
+    socat TCP-LISTEN:$lport,bind=127.0.0.1 UNIX-CONNECT:"$sock" &
 else
     echo "Error: can't connect to X server on display '$ndisp'". 1>&2
     exit 1
 fi
 socat_pid="$!"
-trap "/bin/kill -HUP \"\$socat_pid\" 2>/dev/null" INT QUIT EXIT 
+use_cookie="$False"
 
 # Or add to key: command="sockauth=\"$(mktemp --tmpdir sockauth.XXXXXX)\"; export XAUTHORITY=\"$sockauth\"; export DISPLAY=127.0.0.1:9; xauth nmerge -; eval \"$SSH_ORIGINAL_COMMAND\"" 
-remoteauth="$(mktemp --tmpdir remoteauth.XXXXXX)"
-# Hardcoded port on remote side.
-rport=6009
-rdisp=9
-xauth -f "$remoteauth" generate :0 . trusted timeout 50
-xauth -f "$remoteauth" nlist \
-    | cut -d' ' -f9 \
-    | ssh -C -R 6009:127.0.0.1:$lport "$host" \
-	"remoteauth=\"\$(mktemp --tmpdir remoteauth.XXXXXX)\"
-	 export XAUTHORITY=\"\$remoteauth\"
-	 export DISPLAY=127.0.0.1:$rdisp
-	 read d
-	 xauth add ':$rdisp' . \"\$d\"
+
+# I can't check whether this remote port is available or not, so just
+# guessing.
+rdisp=$(($rdisp + $disp_inc))
+rport=$(($rport + $disp_inc))
+echo "Forward remote port $rport to local port $lport."
+
+if [ "$use_cookie" = "$True" ]; then
+    remoteauth="$(mktemp --tmpdir remoteauth.XXXXXX)"
+    xauth -f "$remoteauth" generate :0 . trusted timeout 5
+    echo "Cache auth cookie in $remoteauth."
+fi
+
+if [ "$use_cookie" = "$True" ]; then
+    xauth -f "$remoteauth" nlist \
+	| cut -d' ' -f9
+else
+    :
+fi \
+    | ssh -C -R $rport:127.0.0.1:$lport "$host" \
+	"set -euf
+
+	 remoteauth=''
+	 trap 'rm -vf \"\$remoteauth\"' INT QUIT EXIT
+
+	 export DISPLAY='127.0.0.1:$rdisp'
+	 echo \"Remote: Will connect to display \$DISPLAY.\"
+
+	 if [ '\"$use_cookie\"' = '\"$True\"' ]; then
+	     remoteauth=\"\$(mktemp --tmpdir remoteauth.XXXXXX)\"
+	     export XAUTHORITY=\"\$remoteauth\"
+	     echo \"Remote: Will use auth cookie from \$XAUTHORITY.\"
+	     read d
+	     xauth add ':$rdisp' . \"\$d\"
+	 fi
+
 	 $*
-	 rm -f \"\$remoteauth\"
 	 "
 # When ssh runs in the background (with '-f'), command it runs can't read
 # stdin.
 
-rm -f "$remoteauth"
-# socat will be killed by `trap` action,
+# socat will be killed and auth cache file removed by `trap` action,
 
